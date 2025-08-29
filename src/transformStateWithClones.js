2cb7494d9d2da1b987712f5e02b397d7107d37b6
Ferens Sofia <ferenssofia3009@gmail.com>
2025-08-29 22:38:57 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,31 +7,48 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const result = [];
-  let currentState = { ...state };
+  const stateHistory = [];
+  let currentState = { ...state }; // клон початкового стану
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
+    let nextState = { ...currentState }; // створюємо копію для оновлень
+
+    switch (action.type) {
+      case 'clear':
+        nextState = {}; // просто очищаємо
+        break;
+
+      case 'addProperties':
+        if (
+          action.extraData &&
+          typeof action.extraData === 'object' &&
+          !Array.isArray(action.extraData)
+        ) {
+          nextState = { ...nextState, ...action.extraData };
+        } else {
+          throw new Error(`Invalid extraData: ${action.extraData}`);
+        }
+        break;
+
+      case 'removeProperties':
+        if (Array.isArray(action.keysToRemove)) {
+          for (const key of action.keysToRemove) {
+            delete nextState[key];
+          }
+        } else {
+          throw new Error(`Invalid keysToRemove: ${action.keysToRemove}`);
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
-    if (action.type === 'removeProperties') {
-      const newState = { ...currentState };
-
-      for (const key of action.keysToRemove) {
-        delete newState[key];
-      }
-      currentState = newState;
-    }
-
-    if (action.type === 'clear') {
-      currentState = {};
-    }
-
-    result.push({ ...currentState });
+    stateHistory.push(nextState);
+    currentState = nextState; // оновлюємо для наступної ітерації
   }
 
-  return result;
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
